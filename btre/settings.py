f12acb62eef1280e4a2852f1b738da69048567b1
Kedar <mr.kedardeshmukh@gmail.com>
2023-06-19 09:20:53 +0530

    local settings

--- btre/settings.py
+++ btre/settings.py
@@ -166,3 +166,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'mr.kedardeshmukh@gmail.com'
 EMAIL_HOST_PASSWORD = 'Kedar@email'
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
